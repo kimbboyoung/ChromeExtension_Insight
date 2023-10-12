8c9fbca779238e4ef5771bb815f457b63e385cb0
jeongwon <rladnjs15@naver.com>
2023-10-12 19:05:31 +0900

    content.js 수정

--- public/content.js
+++ public/content.js
@@ -37,54 +37,53 @@ const getAllCoupangImages = () => {
       }
     });
   }
+  // 1. "prod-buy" 태그 선택
+  const prodBuyElement = document.querySelector(".prod-buy");
+
+  if (prodBuyElement) {
+    // 2. h2 태그 내의 텍스트 추출
+    const h2Element = prodBuyElement.querySelector("h2");
+    const h2Text = h2Element.textContent.trim();
+
+    // 3. "total-price", "prod-shipping-fee-message", "prod-reward-cash-container" 클래스를 가진 하위 요소 선택
+    const totalPriceElement = prodBuyElement.querySelector(".total-price");
+    const shippingFeeElement = prodBuyElement.querySelector(
+      ".prod-shipping-fee-message"
+    );
+    const rewardCashContainerElement =
+      prodBuyElement.querySelector(".prod-reward-cash");
+    const rewards = rewardCashContainerElement.querySelectorAll("p");
+    const rewardsTextArray = Array.from(rewards).map((reward) =>
+      reward.textContent.trim()
+    );
+    const combinedRewardsText = rewardsTextArray.join("\n");
+
+    // 텍스트 추출
+    const totalPriceText = totalPriceElement.textContent.trim();
+    const shippingFeeText = shippingFeeElement.textContent.trim();
+    //const rewardCashText = combinedRewardsText.textContent.trim();
+
+    // 이제 h2Text, totalPriceText, shippingFeeText, rewardCashText에 원하는 텍스트가 들어있습니다.
+    console.log("h2Text:", h2Text);
+    console.log("totalPriceText:", totalPriceText);
+    console.log("shippingFeeText:", shippingFeeText);
+    console.log("combinedRewardsText:", combinedRewardsText);
+
+    // 텍스트를 줄바꿈 문자('\n')로 연결
+    combinedText = [
+      h2Text,
+      totalPriceText,
+      shippingFeeText,
+      combinedRewardsText,
+    ].join("\n");
+    console.log("combinedText:", combinedText);
+  }
 };
 
 // 현재 페이지의 URL 가져오기
 const currentURL = window.location.href;
 
 console.log("현재 페이지 URL:", currentURL);
-
-// 1. "prod-buy" 태그 선택
-const prodBuyElement = document.querySelector(".prod-buy");
-
-if (prodBuyElement) {
-  // 2. h2 태그 내의 텍스트 추출
-  const h2Element = prodBuyElement.querySelector("h2");
-  const h2Text = h2Element.textContent.trim();
-
-  // 3. "total-price", "prod-shipping-fee-message", "prod-reward-cash-container" 클래스를 가진 하위 요소 선택
-  const totalPriceElement = prodBuyElement.querySelector(".total-price");
-  const shippingFeeElement = prodBuyElement.querySelector(
-    ".prod-shipping-fee-message"
-  );
-  const rewardCashContainerElement =
-    prodBuyElement.querySelector(".prod-reward-cash");
-  const rewards = rewardCashContainerElement.querySelectorAll("p");
-  const rewardsTextArray = Array.from(rewards).map((reward) =>
-    reward.textContent.trim()
-  );
-  const combinedRewardsText = rewardsTextArray.join("\n");
-
-  // 텍스트 추출
-  const totalPriceText = totalPriceElement.textContent.trim();
-  const shippingFeeText = shippingFeeElement.textContent.trim();
-  //const rewardCashText = combinedRewardsText.textContent.trim();
-
-  // 이제 h2Text, totalPriceText, shippingFeeText, rewardCashText에 원하는 텍스트가 들어있습니다.
-  console.log("h2Text:", h2Text);
-  console.log("totalPriceText:", totalPriceText);
-  console.log("shippingFeeText:", shippingFeeText);
-  console.log("combinedRewardsText:", combinedRewardsText);
-
-  // 텍스트를 줄바꿈 문자('\n')로 연결
-  combinedText = [
-    h2Text,
-    totalPriceText,
-    shippingFeeText,
-    combinedRewardsText,
-  ].join("\n");
-  console.log("combinedText:", combinedText);
-}
 
 // content.js
 
